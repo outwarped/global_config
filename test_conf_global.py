c5b358a4afb345230edaeb41801620efca9e26cc
dzmitry_buhryk <dzmitry.buhryk@oracle.com>
2021-07-27 14:10:22 +0200

    add test for missing imports

---
+++ test_conf_global.py
@@ -0,0 +1,10 @@
+import unittest
+import textwrap
+
+class TestMethods(unittest.TestCase):
+    def test_op01(self):        
+        # from ..configuration import c
+        from configuration import c
+        pass
+        # self.assertEqual(val, res)
+
